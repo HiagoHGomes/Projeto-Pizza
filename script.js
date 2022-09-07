3e8d834dd659d0bfbf2d3d1a86e72e6695f3fe0e
Hiago Henrique Gomes <hiago_h3@hotmail.com>
2022-09-07 12:19:19 -0300

    Update script.js
    Criação evento onclick.

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//Adicionar dois algarimos depois da virgula
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault(); //Previne a ação padrão.(Nesse caso atualizar a pagina.)
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        },150);
+    });//Para adicionar um evento de clique.
    
     c('.pizza-area').append(pizzaItem);
 });
