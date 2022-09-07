3740fa51ce579ba27319750d4bc8a4f8300671f2
Hiago Henrique Gomes <hiago_h3@hotmail.com>
2022-09-07 13:17:27 -0300

    Update script.js
    Adição dos itens do modal.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const cs = (el)=> document.querySelectorAll(el);
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true); // Clonar um item do html.
     //Preencher as informações itempizza
+
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//Adicionar dois algarimos depois da virgula
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -14,6 +16,13 @@ pizzaJson.map((item, index)=>{
 
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault(); //Previne a ação padrão.(Nesse caso atualizar a pagina.)
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');//pegar a chave que foi clicada
+
+        //Eixibir informações do modal:
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
