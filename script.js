1f7ad128b25baed30a0b1f9cf60b09ac7cfafcb6
Hiago Henrique Gomes <hiago_h3@hotmail.com>
2022-09-07 11:53:55 -0300

    Update script.js
    Adições img, preços, nome e descontos

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ const cs = (el)=> document.querySelectorAll(el);
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true); // Clonar um item do html.
     //Preencher as informações itempizza
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//Adicionar dois algarimos depois da virgula
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+   
     c('.pizza-area').append(pizzaItem);
 });
